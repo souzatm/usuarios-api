49053adfea79f6408e2e2fd926d147880d91b177
Thiago Souza <tsouzatm@outlook.com>
2023-11-05 08:18:01 -0300

    Adicionando módulo 01, início do projeto

---
+++ UsuariosAPI/Profiles/UsuarioProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UsuariosAPI.Data.Dtos;
+
+namespace UsuariosAPI.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+        }
+    }
+}
